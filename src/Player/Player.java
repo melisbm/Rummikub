e9f66628bc9f2b901c3b3cba0160ebad5fed2b34
melisbm <miquelangel.melis@gmail.com>
2026-05-13 20:37:32 +0200

    feat: add Player class with name and points

--- src/Player/Player.java
+++ src/Player/Player.java
@@ -4,9 +4,19 @@ import Rummikub.Tile;
 
 public class Player {
 
+    private String name;
     private Tile[] tilesOnHand;
+    private int points = 0;
 
-    public Player(){
+    public Player(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void takeRandomTile(){
 
     }
 }
