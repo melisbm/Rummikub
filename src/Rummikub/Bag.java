577e5bae86c1c92008669b039cf5f437d1e5fe40
melisbm <miquelangel.melis@gmail.com>
2026-05-13 20:37:32 +0200

    feat: add Bag with tile initialization

--- src/Rummikub/Bag.java
+++ src/Rummikub/Bag.java
@@ -1,4 +1,23 @@
 package Rummikub;
 
+import java.util.*;
+
 public class Bag {
+
+    private List<Tile> numberTiles = new ArrayList<>();
+    private List<Tile> jokerTiles = new ArrayList<>();
+
+    public Bag(){
+
+        for(TileColor tileColor : TileColor.values()){
+
+            for(int i = 1; i <= TileType.NUMBER_CAP; i++){
+                numberTiles.add(new Tile(i, tileColor));
+            }
+        }
+
+        for(int i = 1; i <= TileType.JOKERS; i++){
+            jokerTiles.add(new Tile());
+        }
+    }
 }
--- src/Rummikub/TileType.java
+++ src/Rummikub/TileType.java
@@ -1,4 +1,9 @@
 package Rummikub;
 
 public enum TileType {
+
+    NUMBER, JOKER;
+
+    public static final int NUMBER_CAP = 13;
+    public static final int JOKERS = 2;
 }
