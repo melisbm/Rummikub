647d389cbf5b3980381c148bb1f29d21a1de89fe
melisbm <miquelangel.melis@gmail.com>
2026-05-22 16:08:20 +0200

    fix: correct tiles creation and storage in Bag class

--- src/Rummikub/Bag.java
+++ src/Rummikub/Bag.java
@@ -1,23 +1,27 @@
 package Rummikub;
 
+import Rummikub.Tile.Tile;
+import Rummikub.Tile.TileColor;
+import Rummikub.Tile.TileType;
+
 import java.util.*;
 
 public class Bag {
 
-    private List<Tile> numberTiles = new ArrayList<>();
-    private List<Tile> jokerTiles = new ArrayList<>();
+    private List<Tile> tiles = new ArrayList<>();
 
     public Bag(){
 
         for(TileColor tileColor : TileColor.values()){
-
-            for(int i = 1; i <= TileType.NUMBER_CAP; i++){
-                numberTiles.add(new Tile(i, tileColor));
+            for(int i = 0; i < 2; i++) {
+                for (int j = 1; j <= TileType.NUMBER_CAP; j++) {
+                    tiles.add(new Tile(j, tileColor));
+                }
             }
         }
 
         for(int i = 1; i <= TileType.JOKERS; i++){
-            jokerTiles.add(new Tile());
+            tiles.add(new Tile());
         }
     }
 }
