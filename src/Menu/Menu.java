a7fdd89aba73d3c894b15926dad132edeea377aa
melisbm <miquelangel.melis@gmail.com>
2026-05-22 16:10:52 +0200

    refactor: rename GameMode enum in Menu class

--- src/Menu/Menu.java
+++ src/Menu/Menu.java
@@ -1,29 +1,29 @@
 package Menu;
 
 import Console.Console;
-import Rummikub.GameModes;
+import Rummikub.GameMode;
 
 public class Menu {
 
     Console console;
 
-    public Menu(){
+    public Menu() {
         console = new Console();
     }
 
-    public void mainMenu(){
+    public void mainMenu() {
 
         console.println("=== Main Menu ===");
 
         showGameModesMenu();
-        GameModes gameModePick = askGameMode();
+        GameMode gameModePick = askGameMode();
 
         int numberOfPlayers = askHowManyPlayers(gameModePick);
 
         console.close();
     }
 
-    private void showGameModesMenu(){
+    private void showGameModesMenu() {
 
         console.println("1. Rummikub classic");
         console.println("2. Rummy");
@@ -32,30 +32,30 @@ public class Menu {
 
     }
 
-    private GameModes askGameMode(){
+    private GameMode askGameMode() {
 
         int pick = console.inputInt(">");
 
-        while (pick < 1 || pick > 4){
+        while (pick < 1 || pick > 4) {
 
             String pickOutOfRangeMessage = "Invalid input, type a number between 1 and 4: ";
             pick = console.inputInt(pickOutOfRangeMessage);
         }
 
-        return switch (pick){
-            case 1 -> GameModes.CLASSIC;
-            case 2 -> GameModes.RUMMY;
-            case 3 -> GameModes.GIN;
-            case 4 -> GameModes.ARGENTINIAN;
+        return switch (pick) {
+            case 1 -> GameMode.CLASSIC;
+            case 2 -> GameMode.RUMMY;
+            case 3 -> GameMode.GIN;
+            case 4 -> GameMode.ARGENTINIAN;
             default -> throw new IllegalArgumentException("Game Mode pick must be in the range between 1 and 4.");
         };
     }
 
-    private int askHowManyPlayers(GameModes gameMode){
+    private int askHowManyPlayers(GameMode gameMode) {
 
         int pick = console.inputInt("How many players are going to play: ");
 
-        while (pick < gameMode.getMinPlayers() || pick > gameMode.getMaxPlayers()){
+        while (pick < gameMode.getMinPlayers() || pick > gameMode.getMaxPlayers()) {
 
             String pickOutOfRangeMessage = "Invalid input, type a number between 1 and 4: ";
             pick = console.inputInt(pickOutOfRangeMessage);
@@ -63,5 +63,4 @@ public class Menu {
 
         return pick;
     }
-
 }
